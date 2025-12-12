8e63c6fa997befcc612e5ab05bb656ee903406a9
Alexa Sheldon <alexarsheldon@gmail.com>
2025-12-11 23:45:44 -0500

    Adding functionality to translate snapshot heatmaps over time to GIFS - also added pycache and DS Store to gitignore - can now run snaps_to_gifs.py after running q_agent.py to re-generate new gifs

---
+++ snaps_to_gifs.py
@@ -0,0 +1,34 @@
+from PIL import Image
+import glob
+
+def create_gif_from_pngs(output_filename, png_dir, duration=100, loop=0):
+    """
+    Save the images as a GIF.
+    
+    args:
+      output_filename: Name of the output GIF file.
+      png_dir: Directory containing the PNG files.
+      duration: Duration between frames in milliseconds.
+      loop: Number of loops for the GIF (0 means infinite).
+    """
+    files = glob.glob(f"{png_dir}/*.png")
+
+    if not files:
+        print("No PNG files found in the specified directory.")
+        return
+
+    # Open the first image and append the rest
+    images = [Image.open(file) for file in files]
+    
+    images[0].save(
+        output_filename,
+        save_all=True,
+        append_images=images[1:],
+        duration=duration,
+        loop=loop
+    )
+    print(f"GIF successfully created: {output_filename}")
+
+if __name__ == "__main__":
+    create_gif_from_pngs("evolution.gif", "evolution_snaps", duration=500,loop=0)
+    create_gif_from_pngs("qdiffs.gif", "diff_heatmap_snaps", duration=500,loop=0)
